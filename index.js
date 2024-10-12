60c996666dcb32b118fb9be9f2204d05faef1a6c
Martin Ng <martinng2002@gmail.com>
2024-10-12 23:35:59 +0800

    searoute and firebase

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const axios = require('axios');
 const app = express();
 // Routes
 const vesselRoutes = require('./routes/vessels');
+const { fetchSeaRoute } = require("./searoute");
 const fetchNewsByDateAndCategory = require('./service/NewsService')
 //import { fetchNewsByDateAndCategory } from './service/NewsService';
 
@@ -19,6 +20,7 @@ app.get('/', (req, res) => {
   res.send('Maritime Port Management API is running.');
 });
 
+app.get("/sea-route", fetchSeaRoute);
 
 
 // Route paths
